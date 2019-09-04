27291c58b96235d1ac8fbbf13888db38e00e6d5a
naufalalimw <52844969+naufalalimw@users.noreply.github.com>
2019-09-04 22:46:51 +0700

    Add files via upload

---
+++ point2/point_driver.c
@@ -0,0 +1,14 @@
+/* Nama File : point_driver.c*/
+/* Tanggal 04 September 2019*/
+/* Implementasi ADT Point */
+
+#include "point.h"
+#include <stdio.h>
+int main (){
+    POINT P1 , P2;
+    P2 = MakePOINT(2.34,6.75);
+    BacaPOINT(&P1);
+    TulisPOINT (P1);
+    printf ("\nEQ: %d\n",EQ(P1,P2));
+    return 0;
+}
